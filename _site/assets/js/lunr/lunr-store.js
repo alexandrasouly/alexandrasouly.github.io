b65f6e8c23bac9d6903faf995d1c91e36568ec00
szantamano <alexandrasouly@yahoo.fr>
2021-03-08 23:13:34 +0000

    links are working

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,13 +1,7 @@
 var store = [{
-        "title": "Post: Standard",
-        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["blog"],
-        "tags": ["Post Formats","readability","standard"],
-        "url": "/blog/post-standard/",
-        "teaser": null
-      },{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["Blog"],
+        "title": "Placeholder",
+        "excerpt":"Placeholder post, site under construction…. ******************************** You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web...","categories": ["Blog"],
         "tags": ["Jekyll","update"],
-        "url": "/welcome-to-jekyll",
+        "url": "/placeholder",
         "teaser": null
       }]
